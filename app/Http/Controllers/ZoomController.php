4e8aa1d6edb228184532952f19567d60838b470d
sshoukat <sshoukat@getswift.co>
2021-08-03 02:53:38 +0500

    push origin development

--- app/Http/Controllers/ZoomController.php
+++ app/Http/Controllers/ZoomController.php
@@ -26,7 +26,9 @@ class ZoomController extends Controller
         $from_date = date('Y-m-d', strtotime($request->from_date));
         $to_date = date('Y-m-d', strtotime($request->to_date));
         $response = $this->zoomApiCurl($request->token, $from_date, $to_date);
-
+        // echo '<pre>';
+        // print_r($response['meetings']);
+        // exit;
         if ($from_date > $to_date) {
             $data['title'] = 'Get Participant';
             $data['token'] = $request->token;
@@ -40,27 +42,26 @@ class ZoomController extends Controller
         $participantsList = array();
         if ($response) {
             foreach ($response['meetings'] as $key => $value) {
-                $participants = $this->getParticipantesList($value['uuid'], $request->token);
+                $participants = $this->getParticipantesList($value['id'], $request->token);
                 if (isset($participants)) {
+
+                    $participants = collect($participants);
+                    $participants = $participants->unique('id')->all();
                     foreach ($participants as $key => $participant) {
-                        // dd($participant);
                         $newarray = [];
                         $newarray['id'] = $value['id'];
                         $newarray['host'] = $value['host'];
                         $newarray['topic'] = $value['topic'];
                         $newarray['start_time'] = date('d-m-Y h:s A', strtotime($value['start_time']));
-                        $newarray['user_name'] = $participant['user_name'];
-                        $newarray['email'] = isset($participant['email']) ? $participant['email'] : '-';
-
+                        $newarray['user_name'] = $participant['name'];
+                        $newarray['email'] = isset($participant['user_email']) ? $participant['user_email'] : '-';
                         $newarray = array_values($newarray);
                         array_push($participantsList, $newarray);
                     }
                 }
             }
         }
-        // $newarray = array_values($participantsList);
 
-        // dd(array_values($participantsList));
         $data['title'] = 'Get Participant';
         $data['token'] = $request->token;
         $data['from_date'] = $request->from_date;
@@ -107,7 +108,7 @@ class ZoomController extends Controller
         $curl = curl_init();
 
         curl_setopt_array($curl, array(
-            CURLOPT_URL => "https://mighty-bastion-54961.herokuapp.com/partReports/$token/$id",
+            CURLOPT_URL => "https://api.zoom.us//v2/past_meetings/$id/participants",
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => '',
             CURLOPT_MAXREDIRS => 10,
@@ -115,6 +116,9 @@ class ZoomController extends Controller
             CURLOPT_FOLLOWLOCATION => true,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_HTTPHEADER => array(
+                "Authorization: Bearer $token",
+            ),
         ));
 
         $response = curl_exec($curl);
@@ -127,6 +131,6 @@ class ZoomController extends Controller
             return json_decode($err, true);
         }
         $response = json_decode($response, true);
-        return isset($response['participants']) ? $response['participants'] : [];
+        return $response['participants'];
     }
 }
